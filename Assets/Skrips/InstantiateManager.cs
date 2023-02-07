342c7c01236372fa757220b5427a1ab801ecbfa6
JoakimSandstrom <joakim.sandstrom@elev.ga.ntig.se>
2023-02-07 16:19:26 +0100

    l

--- Assets/Skrips/InstantiateManager.cs
+++ Assets/Skrips/InstantiateManager.cs
@@ -10,23 +10,23 @@ public class InstantiateManager : MonoBehaviour
     public GameObject starPrefab;
     public GameObject planetPrefab;
     public List<GameObject> planets = new();
+    System.Random rng = new System.Random();
     public int planetAmount;
     public bool generated;
     int i = 0;
     int n = 0;
     float radius = 10f; // radius of circle in units
-    int num_points = 360;  // adjust based on desired tradeoff of performance vs. quality
+    int numPoints = 360;  // adjust based on desired tradeoff of performance vs. quality
     
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Vector3[] points = new Vector3[num_points];
-        RotateAround rotate = planetPrefab.GetComponent<RotateAround>() as RotateAround;
+        Vector3[] points = new Vector3[numPoints];
+        //RotateAround rotate = planetPrefab.GetComponent<RotateAround>() as RotateAround;
         LineRenderer lineRenderer = planetPrefab.GetComponent<LineRenderer>() as LineRenderer;
-
-        lineRenderer.positionCount = num_points;
+        lineRenderer.positionCount = numPoints;
 
         if (!generated)
         {
@@ -38,16 +38,16 @@ public class InstantiateManager : MonoBehaviour
             {
                 //Add a new planet
                 planets.Add(planetPrefab);
-                rotate.degreesPerSecond = (i+1)/0.05f;
+                //rotate.degreesPerSecond = (i+1)/0.05f;
                 radius = ((float)Math.Pow(i+1,2));
-                for (n = 0; n < num_points; n++) {
-                    var angle = (Mathf.PI * 2f) * ((float)n / num_points);
+                for (n = 0; n < numPoints; n++) {
+                    var angle = (Mathf.PI * 2f) * ((float)n / numPoints);
                     points[n] = new Vector3(Mathf.Sin(angle) * radius, 0f, Mathf.Cos(angle) * radius);
                 }
                 lineRenderer.SetPositions(points);
                 planets[i].name = "Planet " + i.ToString();
                 planets[i].transform.localScale = new Vector3(i+1,i+1,i+1);
-                planets[i] = Instantiate(planetPrefab, new Vector3(radius,0f,0f), Quaternion.Euler(0f, 0f, 0f));
+                planets[i] = Instantiate(planetPrefab, points[rng.Next(numPoints)], Quaternion.Euler(0f, 0f, 0f));
             }
         }
         else
