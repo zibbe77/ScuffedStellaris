515d2a4275e8d03b90660c0b56be91be07e152cf
theo <theo@zibell.se>
2023-02-09 19:32:16 +0100

    Testade att flyta ui elment med kod

--- Assets/Skrips/MoveHud.cs
+++ Assets/Skrips/MoveHud.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class MoveHud : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private RectTransform transform;
+    public Vector3 setTransform;
 
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        transform = GetComponent<RectTransform>();
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = setTransform;
+        transform.anchoredPosition = new Vector2(setTransform.x, setTransform.y);
     }
 }
