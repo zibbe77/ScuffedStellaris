6f7fd590b04bced3dc6385a3b93628b613bd2ab7
JoakimSandstrom <joakim.sandstrom@elev.ga.ntig.se>
2023-02-23 14:24:06 +0100

    fd

--- Assets/Skrips/InstantiateManager.cs
+++ Assets/Skrips/InstantiateManager.cs
@@ -74,7 +74,6 @@ public class InstantiateManager : MonoBehaviour
 
                 //Set rotation and radius
                 rotate.degreesPerSecond = 30f;
-                //radius = (float) Math.Pow((i+1)*8,1.4);
                 radius = (float)(starPrefab.transform.localScale.x * 2 * (i+1));
 
                 //Set planet type; Only ice if far away from sun;
